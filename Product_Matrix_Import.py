bf13ca86a262c8653f584d03107ffb31dfb383ff
invictuscp <wwulbern@invictuscp.com>
2017-04-27 16:25:44 -0400

    Update Product_Matrix_Import.py

--- Product_Matrix_Import.py
+++ Product_Matrix_Import.py
@@ -372,7 +372,7 @@ def importRateSheets():
 
  # INVESTOR PI MATRIX
  	print "Loading Verus Investor PI Matrix..."
- 	wb = load_workbook(filename = '/home/maverick/VerusPricer/static/matrices/Investor PI Matrix.xlsx')
+ 	wb = load_workbook(filename = '/home/invictus/VerusPricer/static/matrices/Investor PI Matrix.xlsx')
  	ws = wb.active
 	
  	#Load DSCR/No Ratio Matrices
@@ -408,7 +408,7 @@ def importRateSheets():
 
  # FOREIGN NATIONAL MATRIX
  	print "Loading Verus Foreign National Matrix..."
- 	wb = load_workbook(filename = '/home/maverick/VerusPricer/static/matrices/Foreign National Matrix.xlsx')
+ 	wb = load_workbook(filename = '/home/invictus/VerusPricer/static/matrices/Foreign National Matrix.xlsx')
  	ws = wb.active
 	
  	#Load Full Doc Matrices
